fc30cce9e0a11ab3e8ee4aa571a1165b57f55703
Yucen Liu <yucen.u@gmail.com>
2021-07-22 17:55:35 +0000

    display a count for products && categories in the database on the dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME_KEY'], password: ENV['ADMIN_PASSWORD_KEY']
 
   def show
+    @sum_product = Product.count
+    @sum_category = Category.count 
   end
 end
